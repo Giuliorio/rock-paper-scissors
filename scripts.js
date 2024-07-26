a0c5ae54fe57b2ee5836f355181070081c5a9bcb
Giulio Riondino <riondino.giulio@gmail.com>
2024-07-25 21:35:36 -0700

    Implement the computer choice logic

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const randomValue = Math.floor(Math.random() * (9) + 1);
+    
+    if (randomValue <= 3) return "Rock";
+    if (randomValue >= 7) return "Paper";
+    return "Scissors";
+}
